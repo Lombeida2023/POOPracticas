2a2b182af01317070ce47e22441bab1f77ae74c2
Lombeida2023 <132691730+Lombeida2023@users.noreply.github.com>
2023-06-29 23:11:14 -0500

    Add files via upload
    U2LombeidaAnaP9

---
+++ U2LombeidaAnaP9/src/u2lombeidaanap9/Persona.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package u2lombeidaanap9;
+
+/**
+ *
+ * @author LOMBEIDA ANA
+ */
+public class Persona {
+private String nombre;
+private String apellido;
+private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
